8e36c394e54775347bef97ea53ab54c6a71d2210
XIAOZHAOXSXH <112842045+XIAOZHAOXSXH@users.noreply.github.com>
2025-12-30 14:32:31 +0800

    Update diy-part1.sh to modify feed sources
    Uncomment helloworld feed source and add kenzok8 sources.

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# 1. 取消注释 Lede 自带的 helloworld 源 (里面包含一些常用依赖，建议开启)
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# 2. 添加 kenzok8 的第三方源
+# 这个源是目前 OpenWrt 社区最全的源之一，包含 EasyTier 和优化过的 OpenVPN
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
